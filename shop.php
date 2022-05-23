3157f63c3d6fba55f0136e5380fc40384274b34c
xflipperkast <94675097+xflipperkast@users.noreply.github.com>
2022-05-23 13:15:53 +0200

    E
    E

--- shop.php
+++ shop.php
@@ -26,6 +26,7 @@
 
     <!--linking a .css page-->
     <link rel="stylesheet" type="text/css" href="./css/shop.css">
+    <script src="https://kit.fontawesome.com/63f832f2f2.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -39,6 +40,37 @@
             <a href="trade.php">Trading</a>
         </nav>
     </header>
+<section id="felx">
+    <section class="box1">
+    <p>pokemon pack 1</p>
+    <p>€10,-</p>
+    <button>Add</button>
+    </section>
+
+    <section class="box2">
+    <p>pokemon pack 2</p>
+    <p>€15,99</p>
+    <button>Add</button>
+    </section>
+
+    <section class="box3">
+    <p>pokemon pack 3</p>
+    <p>€15,99</p>
+    <button>Add</button>
+    </section>
+    <section class="cart">
+    <button ><i class="fa-solid fa-basket-shopping"></i></button>
+    </section>
+
+</section>
+
+
+
+
+
+
+
+
     <footer>
         <!--footer data-->
 
