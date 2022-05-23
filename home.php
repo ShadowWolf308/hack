980cfe4ea6c7498dfa56b99a961a7d6d5f05e1b9
ShadowWolf308 <levyvandervalk@gmail.com>
2022-05-23 14:17:27 +0200

    .

--- home.php
+++ home.php
@@ -1,50 +1,61 @@
 <!doctype html>
 <html>
-
 <head>
     <!--meta data-->
     <!--usable characters for the website-->
     <meta charset="utf-8">
-
     <!--language of the page-->
     <meta http-equiv="language" content="NL">
-
     <!--display settings-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!--website description-->
     <meta name="description" content="">
-
     <!--author data-->
     <meta name="author" content="Levy van der Valk">
-
     <!--search words for google-->
     <meta name="keywords" content="">
-
     <!--website title in tab-->
     <title>home</title>
-
     <!--linking a .css page-->
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://kit.fontawesome.com/63f832f2f2.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
     <!--body data-->
-    <header>
+    <header id="he">
         <!--header data-->
-        <img src="./images/Logo.png">
+        <img src="./images/logo.png" width="" id="logo">
         <nav>
-            <a href="shop.php">Webshop</a>
+            <a id="nav" href="shop.php">Webshop</a>
         </nav>
     </header>
-    <a href="inloggen.php">inloggen</a>
-    <a href="registreren.php">registreren</a>
+
+    <section id="sect">
+        <article id="intro">
+            <p>Waarom moet je inloggen?</br>
+            we hebben een acount nodig om jou gegevens zoals: kaarten, bezorg adres en je scores op te slaan.</br> 
+            je kan een van deze knoppen gebruiken om verder te gaan.</br>
+            <article id="inlog">
+            <a href="inloggen.php" class="inlog">inloggen</a>
+            <a href="registreren.php" class="inlog">registreren</a>
+            </article>
+        </article>
+        <img src="./images/logo.png" width="" id="img">
+    </section>
+
+
     <footer>
         <!--footer data-->
-
+        <img src="./images/logo.png" id="foot-img">
+        <p id="copy"><b>COPYRIGHT POKESHOP 2022</b></p>
+        <article class="icons">
+        <img src="./images/Laag 4.png">
+        <img src="./images/Laag 3.png">
+        <img src="./images/Laag 2.png">
+        </artcile>
     </footer>
     <!--linking a .js file-->
     <script src="" type="text/javascript"></script>
 </body>
-
 </html>
+ 
