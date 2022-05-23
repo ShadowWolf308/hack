1c3d39373f4406af1135c09eb0611920372c2d38
ShadowWolf308 <levyvandervalk@gmail.com>
2022-05-23 14:50:08 +0200

    .

--- pack.php
+++ pack.php
@@ -47,8 +47,56 @@ if(!$_SESSION['ingelogd']) {
         </nav>
     </header>
     <p>jou packs</p>
-    <section id="pack1">
-
+    <section id="box">
+        <section id="pack1">
+            <button id="pack1img">
+                <img src="./images/pack1.jpg" class="packimg">
+            </button>
+        </section>
+        <section id="pack2">
+            <button id="pack2img">
+                <img src="./images/pack2.jpg" class="packimg">
+            </button>
+        </section>
+        <section id="pack3">
+            <button id="pack3img">
+                <img src="./images/pack3.jpg" class="packimg">
+            </button>
+        </section>
+    </section>
+    <section id="box2">
+        <section>
+            <article>
+                <span id="pokeName1">bulbasaur</span> <br>
+                <span id="pokeType1">grass & poison</span> <br>
+            </article>
+            <article>
+                <span id="pokeName2">bulbasaur</span> <br>
+                <span id="pokeType2">grass & poison</span> <br>
+            </article>
+            <article>
+                <span id="pokeName3">bulbasaur</span> <br>
+                <span id="pokeType3">grass & poison</span> <br>
+            </article>
+            <article>
+                <span id="pokeName4">bulbasaur</span> <br>
+                <span id="pokeType4">grass & poison</span> <br>
+            </article>
+        </section>
+        <section>
+            <article>
+                <span id="pokeName5">bulbasaur</span> <br>
+                <span id="pokeType5">grass & poison</span> <br>
+            </article>
+            <article>
+                <span id="pokeName6">bulbasaur</span> <br>
+                <span id="pokeType6">grass & poison</span> <br>
+            </article>
+            <article>
+                <span id="pokeName7">bulbasaur</span> <br>
+                <span id="pokeType7">grass & poison</span> <br>
+            </article>
+        </section>
     </section>
     <footer>
         <!--footer data-->
@@ -61,7 +109,7 @@ if(!$_SESSION['ingelogd']) {
         </artcile>
     </footer>
     <!--linking a .js file-->
-    <script src="" type="text/javascript"></script>
+    <script src="./js/index.js" type="text/javascript"></script>
 </body>
 
 </html>
